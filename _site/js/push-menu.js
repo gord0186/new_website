162b0b06ca68e9cac11410f6bc2f68ceeec56697
gord0186 <rudygordon@fastmail.com>
2018-08-18 15:48:23 -0400

    removed com js

--- _site/js/push-menu.js
+++ _site/js/push-menu.js
@@ -1,2 +1,40 @@
-console.log('Message!');$(function(){$('body').removeClass("noscript")
-$(".toggle-btn, .close-btn").click(function(){toggleNav();});});function toggleNav(){if($(".site-wrapper").attr("data-state")=="slide-closed"){$(".site-wrapper").attr("data-state","slide-open")}else{$(".site-wrapper").attr("data-state","slide-closed")}}
+// Needs latest version of jQuery to run
+
+console.log('Message!');
+
+$(function(){
+  $('body') .removeClass("noscript")
+
+  $(".toggle-btn, .close-btn").click(function(){
+    toggleNav();
+  });
+
+});
+
+function toggleNav(){
+  if ($(".site-wrapper").attr("data-state") =="slide-closed"){
+    //displays nav when closed
+    $(".site-wrapper").attr("data-state", "slide-open")
+  } else {
+    $(".site-wrapper").attr("data-state", "slide-closed")
+  }
+}
+/*
+$(function() {
+  $('body').removeClass('noscript');
+
+  $('.toggle-btn, .close-btn').click(function() {
+    toggleNav();
+  });
+});
+
+function toggleNav() {
+  if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
+    // Display Nav when closed
+    $('.site-wrapper').attr('data-state', 'slide-open');
+  } else {
+    // Hide Nav when open
+    $('.site-wrapper').attr('data-state', 'slide-closed');
+  }
+}
+*/
